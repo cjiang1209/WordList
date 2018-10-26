e480a2f72556cc79274459366bc78e81976e5624
Chuan Jiang <cjiang1209@gmail.com>
2018-10-26 17:25:45 -0500

    Specify the encoding method using command line arguments.

--- Main.cpp
+++ Main.cpp
@@ -5,18 +5,42 @@
 #include "WordList.h"
 
 int main(int argc, char* argv[]) {
-    if (argc != 2){
+    if (argc != 4){
     	return 1;
     }
 
     MEDDLY::initialize();
 
-//    WordList wl(argv[1], EncodingMethod::BINARY, CharSet::FULL);
-//    WordList wl(argv[1], EncodingMethod::BINARY, CharSet::COMPACT);
-    WordList wl(argv[1], EncodingMethod::ONE_HOT, CharSet::FULL);
-//    WordList wl(argv[1], EncodingMethod::ONE_HOT, CharSet::COMPACT);
-    wl.build();
+    WordList* wl;
+    if (strcmp(argv[2], "binary") == 0) {
+    	if (strcmp(argv[3], "full") == 0) {
+    		wl = new WordList(argv[1], EncodingMethod::BINARY, CharSet::FULL);
+    	}
+    	else if (strcmp(argv[3], "compact") == 0) {
+    		wl = new WordList(argv[1], EncodingMethod::BINARY, CharSet::COMPACT);
+    	}
+    	else {
+    		return 1;
+    	}
+    }
+    else if (strcmp(argv[2], "one-hot") == 0) {
+		if (strcmp(argv[3], "full") == 0) {
+			wl = new WordList(argv[1], EncodingMethod::ONE_HOT, CharSet::FULL);
+		}
+		else if (strcmp(argv[3], "compact") == 0) {
+			wl = new WordList(argv[1], EncodingMethod::ONE_HOT, CharSet::COMPACT);
+		}
+		else {
+			return 1;
+		}
+    }
+    else {
+    	return 1;
+    }
 
+    wl->build();
+
+    delete wl;
     MEDDLY::cleanup();
 
     return 0;
